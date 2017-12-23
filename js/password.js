64800232f7411cff62190fb888fe3a002a4a4870
sanjadjak <sanya.dinic@gmail.com>
2017-12-23 16:16:21 +0100

    done js

--- js/password.js
+++ js/password.js
@@ -1,7 +1,32 @@
 $(document).ready(function() {
-    $("#psw").click(function() {
-        function validate {
+    var psw = document.getElementById("psw");
+    $("#psw").keyup(function(e) {
+
+        psw = this.value;
+        var regex = /[!#$%&’*+/=?^_`{|}~-]+/;
+        var number = /[0-9]+/gm;
+        var lowercase = /[A-Z]+/gm;
+        var uppercase = /(?=.*[a-z])/gm;
+
+
+        if (lowercase.test(psw) == true) {
+            $("#one").css("background-color", "#42ff9e")
 
         }
-    });
-})
+        if (uppercase.test(psw) == true) {
+            $("#three").css("background-color", "#42ff9e")
+        }
+        if (number.test(psw) == true) {
+            $("#two").css("background-color", "#42ff9e")
+        }
+        if (regex.test(psw) == true) {
+            $("#five").css("background-color", "#42ff9e")
+        }
+        if (psw.length >= 8) {
+            $("#four").css("background-color", "#42ff9e") &&
+                $("#email").focus()
+        }
+
+    })
+
+});
